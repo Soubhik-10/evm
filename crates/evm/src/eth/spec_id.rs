7c6939adea212d026e33f512f9716ef79ecd83f2
Soubhik Singha Mahapatra <soubhiksmp2004@gmail.com>
2026-07-25 11:10:32 +0530

    fix(evm): activate EIP-8141 at Bogota

--- crates/evm/src/eth/spec_id.rs
+++ crates/evm/src/eth/spec_id.rs
@@ -21,12 +21,10 @@ pub fn spec_by_timestamp_and_block_number<C>(
 where
     C: EthereumHardforks,
 {
-    if chain_spec.is_amsterdam_active_at_timestamp(timestamp) {
-        // EIP-8141 is implemented on top of the Amsterdam execution-specs branch. REVM keeps
-        // the experimental frame-transaction rules in the subsequent Bogota spec so that they
-        // do not leak into unpatched consumers; this EIP-8141 integration branch opts into them
-        // when Amsterdam is active.
+    if chain_spec.is_bogota_active_at_timestamp(timestamp) {
         SpecId::BOGOTA
+    } else if chain_spec.is_amsterdam_active_at_timestamp(timestamp) {
+        SpecId::AMSTERDAM
     } else if chain_spec.is_osaka_active_at_timestamp(timestamp) {
         SpecId::OSAKA
     } else if chain_spec.is_prague_active_at_timestamp(timestamp) {
@@ -87,6 +85,10 @@ mod tests {
 
         fn amsterdam() -> Self {
             Self::from_timestamp_zero(EthereumHardfork::Amsterdam)
+        }
+
+        fn bogota() -> Self {
+            Self::from_timestamp_zero(EthereumHardfork::Bogota)
         }
 
         fn prague() -> Self {
@@ -153,7 +155,8 @@ mod tests {
     }
 
     #[test_case::test_case(FakeHardfork::osaka(), SpecId::OSAKA; "Osaka")]
-    #[test_case::test_case(FakeHardfork::amsterdam(), SpecId::BOGOTA; "Amsterdam with EIP-8141")]
+    #[test_case::test_case(FakeHardfork::amsterdam(), SpecId::AMSTERDAM; "Amsterdam")]
+    #[test_case::test_case(FakeHardfork::bogota(), SpecId::BOGOTA; "Bogota with EIP-8141")]
     #[test_case::test_case(FakeHardfork::prague(), SpecId::PRAGUE; "Prague")]
     #[test_case::test_case(FakeHardfork::cancun(), SpecId::CANCUN; "Cancun")]
     #[test_case::test_case(FakeHardfork::shanghai(), SpecId::SHANGHAI; "Shanghai")]
